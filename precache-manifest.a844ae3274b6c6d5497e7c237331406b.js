34adf5bcf037ffcfb180c2586848815d12a9dfe6
Z.Sun <szxfly1989@gmail.com>
2024-10-01 15:06:47 -0700

    Updates

---
+++ precache-manifest.a844ae3274b6c6d5497e7c237331406b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425c9ee1ff4bd2061e00eda3e717752b",
+    "url": "/Capstone-Project-/index.html"
+  },
+  {
+    "revision": "8d7170b808dc102f784e",
+    "url": "/Capstone-Project-/static/css/main.02c4959d.chunk.css"
+  },
+  {
+    "revision": "99e5309882a454542203",
+    "url": "/Capstone-Project-/static/js/2.a36e3ff3.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/Capstone-Project-/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "8d7170b808dc102f784e",
+    "url": "/Capstone-Project-/static/js/main.2992ddd0.chunk.js"
+  },
+  {
+    "revision": "4bc40ffc553dc6cb0430",
+    "url": "/Capstone-Project-/static/js/runtime~main.42bf7c97.js"
+  }
+]);
