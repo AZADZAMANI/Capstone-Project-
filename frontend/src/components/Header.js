b98a632f063bd6ad7dc37b21c97d5cf8c82cd9f1
Z.Sun <szxfly1989@gmail.com>
2024-10-11 15:59:04 -0700

    logo added on header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import logo from './logo192.png';
 
 function Header() {
   return (
     <header className="header">
-      <Link to="/" className="title-link">
-        <div className="title">Destination Health</div>
-      </Link>
+      <div className="logo-container">
+        <img src={logo} alt="Destination Health Logo" className="logo" />
+        <Link to="/" className="title-link">
+          <span className="title">Destination Health</span>
+        </Link>
+      </div>
+
       <nav className="nav">
         <Link to="/">Home</Link>
         <Link to="#how-to-book">How to book</Link>
